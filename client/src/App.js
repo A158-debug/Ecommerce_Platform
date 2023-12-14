0f605316af43fb0b9d8f91528d7a839ac9e785c4
A158-debug <kumar.240@iitj.ac.in>
2023-12-14 12:32:41 +0530

    Add Cart Page

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route  } from 'react-router-dom';
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
-
+import CartPage from './pages/CartPage'
 
 const App = () => {
   return (
@@ -13,6 +13,7 @@ const App = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
+            <Route path="/cart" element={<CartPage />} />
           </Routes>
       </BrowserRouter>
     </>

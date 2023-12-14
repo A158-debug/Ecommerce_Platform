a62b556ff79fa606b0016c429be80156a0c6233e
A158-debug <kumar.240@iitj.ac.in>
2023-12-14 12:05:55 +0530

    Add Navbar

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import NavBar from "../features/navbar/Navbar";
+// import ProductList from "../features/product-list/ProductList";
+import NavBar from "../components/Navbar/Navbar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavBar/>
+        {/* <ProductList></ProductList> */}
+      {/* </NavBar> */}
+    </div>
+  );
+};
+
+export default HomePage;
